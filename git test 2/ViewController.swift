89a4efc665e2120fc96ec22db04ea682abe4b618
runseld <runseld2@gmail.com>
2014-06-24 19:55:06 -0500

    Initial Commit

---
+++ git test 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git test 2
+//
+//  Created by Robert Unseld on 6/24/14.
+//  Copyright (c) 2014 Robert Unseld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
